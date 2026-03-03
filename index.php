ca05543072c4a0f5ddfe7d188083aebee2a74ae8
pitiku <pitiku@gmail.com>
2026-03-03 20:57:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// mysql --comments -u '2p7TUipr1WHHH3f.root' -h gateway01.eu-central-1.prod.aws.tidbcloud.com -P 4000 -D 'BS' --ssl-mode=VERIFY_IDENTITY --ssl-ca=<CA_PATH> -p'<PASSWORD>'
-    
 // 1. Configuración de conexión (Sácalo de TiDB -> Connect)
 $host = 'gateway01.eu-central-1.prod.aws.tidbcloud.com';
 $user = '2p7TUipr1WHHH3f.root';
@@ -18,22 +16,30 @@ $conn = mysqli_init();
 // Usamos la ruta de certificados por defecto de Railway
 mysqli_ssl_set($conn, NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
 
-if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL)) {
+if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL))
+{
     die("Error de conexión: " . mysqli_connect_error());
 }
 
 // 4. Lógica para Unity
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST')// && !empty($_POST['nombre']))
+{
     $stmt = $conn->prepare("INSERT INTO 0_user (user_name, is_steam_name) VALUES ('ra', 1)");
     //$stmt->bind_param("si", $nombre, $puntos);
 
-    if ($stmt->execute()) {
+    if ($stmt->execute())
+    {
         echo $conn->insert_id; // ESTO ES LO QUE RECIBE UNITY
-    } else {
+    }
+    else
+    {
         echo "Error en ejecución: " . $stmt->error;
     }
+
     $stmt->close();
+}
 
 $conn->close();
 ?>
+
 
