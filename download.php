f24b9bfd48b2106613645d10863caf8a702ea79c
pitiku <pitiku@gmail.com>
2026-03-11 13:03:40 +0100

    Update download.php

--- download.php
+++ download.php
@@ -5,12 +5,17 @@ $host = 'gateway01.eu-central-1.prod.aws.tidbcloud.com';
 $user = '2p7TUipr1WHHH3f.root';
 $pass = '5ZcNOCkyQA9VGvfL';
 $db   = 'BS';
-$port = 4000;
 $dsn = "mysql:host=$host;dbname=$db;port=4000;charset=utf8mb4";
 
 try {
-    $pdo = new PDO($dsn, $user, $pass, [PDO::MYSQL_ATTR_SSL_CA => '', PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false]);
-
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::MYSQL_ATTR_SSL_CA => '', 
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+    ];
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    
     $tabla = $_GET['tabla'];
     $columna = $_GET['columna'];
     $id_campo = $_GET['id_campo']; // Nombre de la columna clave primaria
